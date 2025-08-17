6ec6270cd4d87450cd2cbb5b1edceeacd0444355
Jaydeep <workthunder33@gmail.com>
2025-08-18 01:12:04 +0530

    nothing to add

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 // Routes
 app.use("/api/mock", mockRoutes);  // Mock API
-app.use("/api/aps", apsRoutes);    // APS API (later)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
